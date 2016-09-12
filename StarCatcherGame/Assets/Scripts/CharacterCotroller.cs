3bb342b53bf8c59dbd53c13912eafd91da11ed9e
brytoncl <brytonclittle@gmail.com>
2016-09-12 14:01:12 -0600

    Imported Scripts

---
+++ StarCatcherGame/Assets/Scripts/CharacterCotroller.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterCotroller : MonoBehaviour {
+
+	//this is the character controller component
+	private CharacterController myCC; 
+	private Vector3 tempPos;
+
+	public float speed;
+	public float jumpHeight;
+	public float gravity;
+
+	//temp Var of datatype vector3 to moce the character.
+
+	void Strafe () {
+		
+	}
+
+	void Start () {
+		//this finds the character component
+		myCC = GetComponent<CharacterController> ();
+
+	}
+
+	void Update () {
+		
+			
+	
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			tempPos.y = jumpHeight;
+		}
+
+		tempPos.y -= gravity;
+		tempPos.x = speed * Input.GetAxis("Horizontal");
+		myCC.Move(tempPos * Time.deltaTime);
+
+		
+	}
+}
---
+++ StarCatcherGame/Assets/Scripts/Movement.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+	public float moveSpeed;
+	public float jumpHeight; 
+
+	public Transform groundPoint;
+	public float radius;
+	public LayerMask groundMask;
+	public int extraJumps;
+	int DefaultExtraJumps;
+
+	bool isGrounded;
+
+
+	public Rigidbody2D rb; 
+
+	void Move () {
+		Vector2 moveDir = new Vector2 (Input.GetAxisRaw ("Horizontal") * moveSpeed * Time.deltaTime, rb.velocity.y);
+		rb.velocity = moveDir;
+	}
+
+	void Jump () {
+		CheckGrounded ();
+		if(Input.GetKeyDown(KeyCode.Space) && isGrounded){
+			rb.AddForce(new Vector2 (0, jumpHeight));
+		}
+	}
+	void DoubleJump (){
+		if(Input.GetKeyDown(KeyCode.Space) && extraJumps >= 1 && !isGrounded){
+			rb.AddForce(new Vector2 (0, jumpHeight));
+			extraJumps -= 1; 
+			}
+		}
+	void CheckGrounded () {
+		isGrounded = Physics2D.OverlapCircle (groundPoint.position, radius, groundMask);
+		if (isGrounded) {
+			extraJumps = DefaultExtraJumps;
+		}
+	}
+
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+		DefaultExtraJumps = extraJumps;
+	}
+		
+	void Update (){
+		Move ();
+		Jump ();
+		DoubleJump ();
+	}
+
+	void OnDrawGizmos () {
+		Gizmos.color = Color.red;
+		Gizmos.DrawWireSphere(groundPoint.position, radius); 
+	}
+}
