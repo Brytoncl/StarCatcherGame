efa5022986eefa3cb77d2bb7edd45fa6f49d91b8
brytoncl <brytonclittle@gmail.com>
2016-09-20 09:49:29 -0600

    quick save

---
+++ StarCatcherGame/Assets/Scripts/HomeWork/ConventionsAndSyntax.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class ConventionsAndSyntax : MonoBehaviour {
+
+	//players health
+	//I have to put f at the end of the assigned variable for a float
+	public float health = 110.5f;
+
+	//the damage being delt to the player;
+	public int damage;
+
+
+
+	void HurtPlayer (){
+		health -=damage;
+		print (health);
+	}
+	// will only hurt player once.
+	void Start () {
+		print (health);
+		HurtPlayer ();
+	}
+
+	// void update wont work because it needs to be a capital U
+	//right now void update(); is a completely different function than void Update();
+	void update () {
+		HurtPlayer ();
+	}
+
+	//this update will work
+	void Update (){
+	
+	}
+}
---
+++ StarCatcherGame/Assets/Scripts/HomeWork/IfAndSwitch.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IfAndSwitch : MonoBehaviour {
+
+
+	public int health;
+
+	public int recover;
+
+	public int damage;
+
+
+	int randomPickup = 2;
+
+
+	void DamagePlayer () {
+		health -= damage;
+		print (health);
+		RecoverHealth ();
+	}
+	//this is a scenario where I would want to have an if statement because I only have a couple of options to deal with
+	void RecoverHealth () {
+		if (health <= 80) {
+			health += recover;
+			print (health);
+			RecoverHealth ();
+
+		} else
+			print ("healht is good");
+	}
+
+	// will perform different actions based on the value of the random pickup
+	//switch statements are good for reacting to multiple changing vaariables its not limited to two options.
+
+	void Action () {
+		switch (randomPickup) {
+		case 1:
+			print ("picked up a health power up");
+			//assignes a random value to random pickup
+			randomPickup = Random.Range (0,4);
+			break;
+		case 2:
+			print ("picked up a speed power up");
+			randomPickup = Random.Range (0,4);
+			break;
+		case 3:
+			print ("picked up a damage power up");
+			randomPickup = Random.Range (0,4);
+			break;
+		case 4:
+			print ("picked up an over shield");
+			randomPickup = Random.Range (0,4);
+			break;
+		default:
+			print ("no active power up");
+			randomPickup = Random.Range (0,4);
+			//I realize there is an error with the random.range but it is doing what i want for now regardless and I would find a solution if I was intending on using this script.
+			break;
+	
+		}
+	}
+	// Use this for initialization
+	void Start () {
+		Action ();
+		print (health);
+		DamagePlayer ();
+		Action ();
+	}
+}
