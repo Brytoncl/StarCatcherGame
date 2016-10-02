f6a7468e61882cedbc2256ffc7771fc09eaef85d
brytoncl <brytonclittle@gmail.com>
2016-10-01 18:15:39 -0600

    Scene Loader

--- StarCatcherGame/Assets/Scripts/LevelSpawning/Statics.cs
+++ StarCatcherGame/Assets/Scripts/LevelSpawning/Statics.cs
@@ -3,5 +3,6 @@ public class Statics {
 
 	public static float distance = 36;
 	public static float nextPosition = 64;
+	public static float startPosition = nextPosition;
 
 }
---
+++ StarCatcherGame/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour {
+
+	void Start () {
+		Statics.nextPosition = Statics.startPosition;
+	}
+
+	void OnTriggerEnter ()
+	{
+		RestartLevel ();
+	}
+
+	public void RestartLevel()
+	{
+		SceneManager.LoadScene (0);
+	}
+}
