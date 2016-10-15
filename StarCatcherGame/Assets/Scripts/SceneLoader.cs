e0caf5fa7236d1750fa84f73689541fe2403b5b3
brytoncl <brytonclittle@gmail.com>
2016-10-15 08:18:27 -0600

    menu loader

--- StarCatcherGame/Assets/Scripts/SceneLoader.cs
+++ StarCatcherGame/Assets/Scripts/SceneLoader.cs
@@ -4,9 +4,13 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour {
 
+
+
+
 	void Start () {
 		Statics.nextPosition = Statics.startPosition;
 	}
+
 
 	void OnTriggerEnter ()
 	{
@@ -16,5 +20,9 @@ public class SceneLoader : MonoBehaviour {
 	public void RestartLevel()
 	{
 		SceneManager.LoadScene (0);
+	}
+
+	public void StartGame (){
+		SceneManager.LoadScene (1);
 	}
 }
