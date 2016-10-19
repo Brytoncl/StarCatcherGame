ca1456466669e76aeb411f1db121460ce7a6c86a
brytoncl <brytonclittle@gmail.com>
2016-10-19 16:15:26 -0600

    Animation Done!

--- StarCatcherGame/Assets/Scripts/Character/Animation.cs
+++ StarCatcherGame/Assets/Scripts/Character/Animation.cs
@@ -3,15 +3,21 @@ using System.Collections;
 
 public class Animation : MonoBehaviour {
 
+	MoveCharacter myMoveCharacter;
 	public Animator myAnimator;
-	private float _h; 
+	private float _h;
+
 
 	void Start () {
+		myMoveCharacter = new MoveCharacter ();
 		myAnimator = GetComponent<Animator> ();
-		_h = Input.GetAxis( "Horizontal");
+		myMoveCharacter.myCC = GetComponent<CharacterController> ();
+
 	}
 
 	void Update () {
+		_h = Mathf.Abs (myMoveCharacter.myCC.velocity.x);
 		myAnimator.SetFloat ("speed", _h);
-	}
+
+}
 }
