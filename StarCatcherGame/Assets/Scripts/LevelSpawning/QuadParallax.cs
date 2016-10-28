b80fe54b2d911d3654d714007259ae3f99b8e595
brytoncl <brytonclittle@gmail.com>
2016-10-28 13:08:52 -0600

    starScripts

---
+++ StarCatcherGame/Assets/Scripts/LevelSpawning/QuadParallax.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QuadParallax : MonoBehaviour {
+
+	public float speed = 0.5f;
+
+	void Update () {
+		Vector2 offset = new Vector2(speed * Time.time, 0);
+		GetComponent<Renderer> ().material.mainTextureOffset = offset;
+	}
+}
